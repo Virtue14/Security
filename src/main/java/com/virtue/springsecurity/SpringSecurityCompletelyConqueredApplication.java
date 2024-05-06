8f401446ef728c002ec561de0af1905aa00ad5c9
악덕 <rudejrqkr97@gmail.com>
2024-05-06 23:20:18 +0900

    :tada: init

---
+++ src/main/java/com/virtue/springsecurity/SpringSecurityCompletelyConqueredApplication.java
@@ -0,0 +1,13 @@
+package com.virtue.springsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityCompletelyConqueredApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityCompletelyConqueredApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/virtue/springsecurity/SpringSecurityCompletelyConqueredApplicationTests.java
@@ -0,0 +1,13 @@
+package com.virtue.springsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityCompletelyConqueredApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
